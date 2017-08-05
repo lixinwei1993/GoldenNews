9242ac6d0cf3a48563b2db0ff51c493fc8f6e4b7
lixinwei <823101347@qq.com>
2017-08-05 22:23:05 +0800

    add splashActivity to improve UX by preLoading data for MainActivity; add extraSpace for the linearLayoutManager of RecyclerView to improve the image loading behavior some other small changes

---
+++ app/src/main/java/com/lixinwei/www/goldennews/splash/SplashActivity.java
@@ -0,0 +1,10 @@
+package com.lixinwei.www.goldennews.splash;
+
+import com.lixinwei.www.goldennews.base.BaseActivity;
+
+/**
+ * Created by welding on 2017/8/5.
+ */
+
+public class SplashActivity extends BaseActivity {
+}
---
+++ app/src/main/java/com/lixinwei/www/goldennews/util/PicassoTransforms.java
@@ -0,0 +1,8 @@
+package com.lixinwei.www.goldennews.util;
+
+/**
+ * Created by welding on 2017/8/5.
+ */
+
+public class PicassoTransforms {
+}
